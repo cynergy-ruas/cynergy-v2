15e96cbc720ed1104b27621767b5700ad5ba73c0
Hitesh Malhotra <47815600+HiteshM2107@users.noreply.github.com>
2020-09-27 21:59:12 +0530

    Initial commit by Hitesh

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import { Hero, Events, About } from '@components';
+import { ContactUs } from '../components';
 
 const IndexPage = () => (
     <Layout>
@@ -10,6 +11,7 @@ const IndexPage = () => (
         <Hero />
         <Events />
         <About />
+        <ContactUs />
     </Layout>
 );
 
